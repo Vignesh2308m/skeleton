a8eef978dc10fee7f75685d3a0fb27ca9b825757
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-06-13 17:23:42 +0530

    Initial exploration

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,12 +1,49 @@
-use std::fs;
+use std::io::{Error, Read};
+use std::fs::{self, File};
+use std::io::BufReader;
 
+use crate::FileType::Txt;
 
-fn main() {
-    let file = fs::read_to_string("./data/intro.txt");
-    if let Err(err) = file {
-        println!("{}", err.to_string());
-        return
+enum FileType {
+    Txt
+}
+
+struct Reader{
+    buf : BufReader<File>,
+    file_type : FileType
+}
+
+impl Reader {
+    fn new(path:&str, file_type:FileType) -> Result<Reader, Error> {
+        let file = fs::File::open(path)?;
+        let buf = BufReader::new(file);
+
+        Ok(Reader{
+            buf:buf,
+            file_type:file_type
+        })
     }
 
-    println!("{}",file.ok().unwrap());
+    fn read(&mut self) -> Result<[u8;10], Error> {
+        let mut content = [0;10];
+        self.buf.read(&mut content)?;
+        Ok(content)
+    }
+}
+
+fn main() {
+    let r = Reader::new("./data/intro.txt", Txt);
+    if let Err(err) = r {
+        println!("{}", err);
+        return;
+    }
+
+    let c = r.ok().unwrap().read();
+
+    if let Err(err) = c {
+        println!("{}", err);
+        return;
+    }
+
+    println!("{:?}", c.ok().unwrap());
 }
