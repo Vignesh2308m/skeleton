6d9f56a139ad6b360618358b75ce46a6f550f0a4
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-06-14 18:39:15 +0530

    Byte Version

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::{BufReader, Read};
 
 
-fn main(){
+fn main()-> Result<(), std::io::Error>{
     // Getting input arguments
     let args:Vec<String> = env::args().collect();
 
@@ -12,7 +12,7 @@ fn main(){
     }
 
     let path = &args[1];
-    let pattern = &args[2];
+    let pattern = &args[2].as_bytes();
 
     //Loading File Buffer
 
@@ -23,9 +23,25 @@ fn main(){
 
     let mut buffer = BufReader::new(file.unwrap());
 
-    let mut place_holder = String::new();
+    let mut place_holder = [0; 1024];
 
-    let _ = buffer.read_to_string(&mut place_holder);
+    // let _ = buffer.read_to_string(&mut place_holder);
 
-    println!("{}", place_holder.contains(pattern));
+    loop {
+        let n = buffer.read(&mut place_holder)?;
+
+        if n == 0{
+            break;
+        }
+
+        for i in 0..n{
+            if &place_holder[i..i+pattern.len()] == *pattern {
+                println!("Match found");
+                break;
+            }
+        }
+    }
+
+    Ok(())
+    // println!("{}", place_holder.contains(pattern));
 }
