eb42f46e0b4111e67f9238cb5f3c7633556b3d89
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-06-16 07:16:32 +0530

    Added line no

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -2,47 +2,65 @@ use std::env;
 use std::fs::File;
 use std::io::{BufReader, Read};
 
-
-fn main()-> Result<(), std::io::Error>{
+fn main() -> Result<(), std::io::Error> {
     // Getting input arguments
-    let args:Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3{
-        panic!("number of argments mismatches");
+    if args.len() != 3 {
+        panic!("number of arguments mismatches");
     }
 
     let path = &args[1];
-    let pattern = &args[2].as_bytes();
+    let pattern = args[2].as_bytes();
 
-    //Loading File Buffer
+    // Loading File Buffer
+    let file = File::open(path)?;
 
-    let file = File::open(path);
-    if let Err(_) = file {
-        panic!("Unable to Open File");
-    }
-
-    let mut buffer = BufReader::new(file.unwrap());
+    let mut buffer = BufReader::new(file);
 
     let mut place_holder = [0; 1024];
 
-    // let _ = buffer.read_to_string(&mut place_holder);
+    let mut line_no: usize = 1;
+    let mut offset: usize = 0;
 
     loop {
         let n = buffer.read(&mut place_holder)?;
 
-        println!("{}", n);
-
-        if n == 0{
+        if n == 0 {
             break;
         }
 
-        for i in 0..n-pattern.len()+1{
-            if &place_holder[i..i+pattern.len()] == *pattern {
-                println!("{},{}",i, i+pattern.len());
+        let chunk = &place_holder[..n];
+
+        let mut window = 0;
+
+        for i in chunk.windows(pattern.len()) {
+            if i == pattern {
+                println!(
+                    "line {}, start {}, end {}",
+                    line_no,
+                    offset + window,
+                    offset + window + pattern.len()
+                );
+            }
+
+            // Move to next line if current byte is '\n'
+            if chunk[window] == b'\n' {
+                line_no += 1;
+            }
+
+            window += 1;
+        }
+
+        // Handle any remaining bytes after the last window
+        for &b in &chunk[window..] {
+            if b == b'\n' {
+                line_no += 1;
             }
         }
+
+        offset += n;
     }
 
     Ok(())
-    // println!("{}", place_holder.contains(pattern));
 }
