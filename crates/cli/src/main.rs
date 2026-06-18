84aabf4e6c5bac8a80d8732abb632bd8811188df
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-06-18 23:04:42 +0530

    Need to work mor on overlap

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -36,59 +36,49 @@ fn get_args() -> Result<Args, std::io::Error> {
 }
 
 fn find_match(mut buf: BufReader<File>, pattern: &[u8])-> Result<Vec<Match>,std::io::Error>{
-    let mut place_holder = [0; 1024];
     let mut matches: Vec<Match> =Vec::new(); 
     let mut line_no: usize = 1;
     let mut offset: usize = 0;
+    let window_size = pattern.len();
 
-    let mut mem_buffer = MemBuffer {
-    place_holder: [0; 1024],
-    overlap: Vec::new(),
-};
-
+    let mut mem_buffer = MemBuffer{
+        place_holder : [0; 1024],
+        overlap : Vec::new()
+    };
+    
     loop {
-    let n = buf.read(&mut mem_buffer.place_holder)?;
+        let n = buf.read(&mut mem_buffer.place_holder)?;
 
-    if n == 0 {
-        break;
-    }
+        let window = 0;
 
-    let chunk = &mem_buffer.place_holder[..n];
-
-    let mut search_buf =
-        Vec::with_capacity(mem_buffer.overlap.len() + chunk.len());
-
-    search_buf.extend_from_slice(&mem_buffer.overlap);
-    search_buf.extend_from_slice(chunk);
-
-    for (idx, win) in search_buf.windows(pattern.len()).enumerate() {
-        if win == pattern {
-            matches.push(Match {
-                line_no,
-                start: offset + idx.saturating_sub(mem_buffer.overlap.len()),
-                end: offset
-                    + idx.saturating_sub(mem_buffer.overlap.len())
-                    + pattern.len(),
-            });
+        if n==0{
+            break;
         }
-    }
 
-    for &b in chunk {
-        if b == b'\n' {
-            line_no += 1;
+        let chunk = &mem_buffer.place_holder[..n];
+
+        let mut w =0;
+        while mem_buffer.overlap.len() > 0 {
+            if (chunk[..w]==pattern[..w]) && (mem_buffer.overlap[w..]== pattern[w..]){
+                matches.push(
+                    Match { line_no:line_no, start: offset+w, end: offset+w+window_size }
+                );
+            }
+            w+=1;
         }
+
+        for i in chunk.windows(window_size){
+            if i == pattern{
+                matches.push(
+                    Match { line_no:line_no, start: offset+w, end: offset+w+window_size }
+                );
+            }
+            w+=1;
+        }
+        offset += n;
     }
 
-    let overlap_len = pattern.len().saturating_sub(1).min(search_buf.len());
-
-    mem_buffer.overlap.clear();
-    mem_buffer
-        .overlap
-        .extend_from_slice(&search_buf[search_buf.len() - overlap_len..]);
-
-    offset += n;
-}
-
+    
     Ok(matches)
  
 }
