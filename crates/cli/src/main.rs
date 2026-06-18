2e6fe2effba8e3b63723e3fd799145b50342451b
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-06-19 01:20:59 +0530

    Overflow issue sorted

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, mem};
 use std::fs::File;
 use std::io::ErrorKind::InvalidInput;
 use std::io::{BufReader, Read};
@@ -14,7 +14,7 @@ struct Match{
 }
 
 struct MemBuffer {
-    place_holder: [u8; 10],
+    place_holder: [u8; 5],
     overlap: Vec<u8>,
 }
 
@@ -36,9 +36,52 @@ fn get_args() -> Result<Args, std::io::Error> {
 }
 
 fn find_match(mut buf: BufReader<File>, pattern: &[u8])-> Result<Vec<Match>,std::io::Error>{
-    // Do it again until get it correct
-    todo!()
- 
+    let mut offset = 0;
+    let mut line_no = 0;
+    let size = pattern.len();
+    let mut matches: Vec<Match> = Vec::new();
+    let mut mem_buf = MemBuffer{
+        place_holder:[0;5],
+        overlap:Vec::new()
+    };
+
+    loop{
+        let n = buf.read(&mut mem_buf.place_holder)?;
+
+        if n == 0{
+            break;
+        }
+        println!("{}", n);
+
+        let chunk = &mem_buf.place_holder[..n];
+        println!("{:?}", mem_buf.overlap);
+        println!("{:?}", chunk);
+
+        let mut temp:Vec<u8> = Vec::new();
+
+        for i in 0..mem_buf.overlap.len(){
+            temp.clear();
+            temp.extend_from_slice(&mem_buf.overlap[i..]);
+            temp.extend_from_slice(&chunk[..i+1]);
+
+            if &temp == pattern{
+                print!("YES");
+            }
+        }
+
+        for i in chunk.windows(size){
+            //println!("{:?}",i);
+            if i == pattern{
+                print!("YES");
+            }
+        }
+        if n>size{
+            mem_buf.overlap.clear();
+            mem_buf.overlap.extend_from_slice(&chunk[n-size+1..]);
+        }
+        //println!("{:?}", mem_buf.overlap);
+    }
+    Ok(matches)
 }
 
 fn print_pretty(matches: Vec<Match>) -> Result<(), std::io::Error>{
@@ -56,7 +99,7 @@ fn main() -> Result<(), std::io::Error> {
     // Loading File Buffer
     let file = File::open(arg.path)?;
 
-    let mut buffer = BufReader::new(file);
+    let buffer = BufReader::new(file);
 
     let matches = find_match(buffer, arg.pattern.as_slice())?; 
     
