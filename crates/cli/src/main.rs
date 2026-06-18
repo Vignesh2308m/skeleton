ff30250cec3053044c9f084879ed55424981192a
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-06-19 00:15:59 +0530

    Todo matching

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -14,7 +14,7 @@ struct Match{
 }
 
 struct MemBuffer {
-    place_holder: [u8; 1024],
+    place_holder: [u8; 10],
     overlap: Vec<u8>,
 }
 
@@ -36,50 +36,8 @@ fn get_args() -> Result<Args, std::io::Error> {
 }
 
 fn find_match(mut buf: BufReader<File>, pattern: &[u8])-> Result<Vec<Match>,std::io::Error>{
-    let mut matches: Vec<Match> =Vec::new(); 
-    let mut line_no: usize = 1;
-    let mut offset: usize = 0;
-    let window_size = pattern.len();
-
-    let mut mem_buffer = MemBuffer{
-        place_holder : [0; 1024],
-        overlap : Vec::new()
-    };
-    
-    loop {
-        let n = buf.read(&mut mem_buffer.place_holder)?;
-
-        let window = 0;
-
-        if n==0{
-            break;
-        }
-
-        let chunk = &mem_buffer.place_holder[..n];
-
-        let mut w =0;
-        while mem_buffer.overlap.len() > 0 {
-            if (chunk[..w]==pattern[..w]) && (mem_buffer.overlap[w..]== pattern[w..]){
-                matches.push(
-                    Match { line_no:line_no, start: offset+w, end: offset+w+window_size }
-                );
-            }
-            w+=1;
-        }
-
-        for i in chunk.windows(window_size){
-            if i == pattern{
-                matches.push(
-                    Match { line_no:line_no, start: offset+w, end: offset+w+window_size }
-                );
-            }
-            w+=1;
-        }
-        offset += n;
-    }
-
-    
-    Ok(matches)
+    // Do it again until get it correct
+    todo!()
  
 }
 
