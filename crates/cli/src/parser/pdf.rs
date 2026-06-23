e4686d3cf5ad7fd08b02ba47695ed47131a7c12b
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-06-23 11:34:51 +0530

    parser api

---
+++ crates/cli/src/parser/pdf.rs
@@ -0,0 +1,19 @@
+
+
+
+struct Pdf{
+    
+}
+
+impl Pdf{
+    fn new(){
+        todo!();
+    }
+    fn read(){
+        todo!();
+    }
+    
+    fn close(){
+        todo!();
+    }
+}
---
+++ crates/cli/src/parser/txt.rs
@@ -0,0 +1,19 @@
+
+
+
+struct Txt{
+    
+}
+
+impl Txt{
+    fn new(){
+        todo!();
+    }
+    fn read(){
+        todo!();
+    }
+    
+    fn close(){
+        todo!();
+    }
+}
