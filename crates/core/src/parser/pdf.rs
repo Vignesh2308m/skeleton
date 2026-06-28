4d7059df0a613b57585432035f5ade99bee85fa8
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-06-28 07:10:48 +0530

    Starting Pdf

--- crates/core/src/parser/pdf.rs
+++ crates/core/src/parser/pdf.rs
@@ -1,19 +1,28 @@
+use std::io::{Error, Read};
+use std::fs::File;
+use std::io::{BufReader};
 
+const SIZE:usize = 4096;
 
-
-struct Pdf{
-    
+struct Pdf{ 
+    file_buffer: BufReader<File>,
+    mem_buffer: Vec<u8>,
+    line_offset: usize,
+    byte_offset: usize, 
 }
 
 impl Pdf{
-    fn new(){
-        todo!();
+    fn new(path:&str)-> Result<Pdf, Error>{
+
+        let file = File::open(path)?;
+
+        let buffer = BufReader::new(file);
+
+        Ok(Pdf{ file_buffer: buffer, mem_buffer:vec![0u8;SIZE], line_offset:0, byte_offset:0})
     }
+
     fn read(){
         todo!();
     }
     
-    fn close(){
-        todo!();
-    }
 }
