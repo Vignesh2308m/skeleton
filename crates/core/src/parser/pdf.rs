059c49b3a82ffe2e255b9baf8f2a0641caffd981
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-07-21 06:57:30 +0530

    Pdf interfacing done

--- crates/core/src/parser/pdf.rs
+++ crates/core/src/parser/pdf.rs
@@ -1,124 +1,73 @@
-use std::collections::HashMap;
-use std::io::{Error, Read, Seek};
 use std::fs::File;
-use std::io::{BufReader};
-use flate2::read::ZlibDecoder;
-use std::convert::TryInto;
-use std::ops::RemAssign;
+use std::io::{BufReader, Error};
 
-const SIZE:usize = 1024;
+use super::{Parser, ParserMetadata};
 
-enum Layout{
-    HEADER,
-    XREFTABLE,
-    STARTXREF,
-    TRAILER,
-    EOF,
-}
+const SIZE: usize = 1024;
 
-struct Pdf{ 
+pub struct Pdf {
+    path: String,
     file_buffer: BufReader<File>,
     mem_buffer: Vec<u8>,
-    xref_table: Vec<usize>,
-    line_offset: usize,
-    byte_offset: usize, 
 }
 
-impl Pdf{
-    fn new(path:&str)-> Result<Pdf, Error>{
-
+impl Pdf {
+    fn open(path: &str) -> Result<Pdf, Error> {
         let file = File::open(path)?;
-
         let buffer = BufReader::new(file);
 
-        Ok(Pdf{ file_buffer: buffer, mem_buffer:vec![0u8;SIZE], xref_table:Vec::new(),line_offset:0, byte_offset:0})
+        Ok(Pdf {
+            path: path.to_string(),
+            file_buffer: buffer,
+            mem_buffer: vec![0u8; SIZE],
+        })
+    }
+}
+
+impl Parser for Pdf {
+    fn new(path: &str) -> Result<Self, Error> {
+        Self::open(path)
     }
 
-    fn read(&mut self)-> Result<&[u8], std::io::Error>{
-        // Go to Eof 
-        let file_size = self.file_buffer.seek(std::io::SeekFrom::End(0))?;
-        let read_size = file_size.min(SIZE as u64) as usize;
-        self.file_buffer.seek(std::io::SeekFrom::End(-(read_size as i64)))?;
-        let n = self.file_buffer.read(&mut self.mem_buffer)?; 
+    fn read(&mut self) -> Result<&[u8], Error> {
+        //let path = self.path.clone();
+        let file = pdf::file::FileOptions::uncached()
+            .open(&self.path)
+            .map_err(|err| Error::new(std::io::ErrorKind::Other, err))?;
 
-        
-        //Finding startxref
-        let sxref = self.bounded_seg_search( b"startxref\r\n", 0, n )?;
-        let eof = self.bounded_seg_search( b"\r\n%%EOF", 0 , n )?;
-        
-        let mut xref_pos:u64 = 0;
-        for i  in &self.mem_buffer[sxref+b"startxref\r\n".len()..eof]{
-            xref_pos = xref_pos*10 + (*i - b'0') as u64;
+        let mut summary = String::new();
+        summary.push_str(&format!("pages: {}\n", file.num_pages()));
+
+        if let Some(ref info) = file.trailer.info_dict {
+            let title = info.title.as_ref().map(|p| p.to_string_lossy());
+            if let Some(t) = title {
+                summary.push_str(&format!("title: {}\n", t));
+            }
         }
 
-        //Finding Xref table
-        self.file_buffer.seek(std::io::SeekFrom::Start(xref_pos))?;
-        let n = self.file_buffer.read(&mut self.mem_buffer)?; 
-        let x = self.bounded_seg_search(b"xref",0,n)?;
-        let y = self.bounded_seg_search(b"trailer",x,n)?;
-
-        let xref_lines = self.nested_seg_search(b"\r\n", x, y)?;
-        let xref_count = self.nested_seg_search(b"\t", xref_lines[0], xref_lines[1])?;
-
-        for i in 1..xref_lines.len()-1{
-            let xref_row = self.nested_seg_search(b" ", xref_lines[i], xref_lines[i+1])?;
-            println!("{:?}", &self.mem_buffer[xref_lines[i]..xref_lines[i+1]]);
-            println!("{}", String::from_utf8_lossy(&self.mem_buffer[xref_lines[i]..xref_lines[i+1]]));
-            self.xref_table.push(self.byte_to_ascii(xref_row[0]+2, xref_row[1]+3)?);
-        }
-        
-        self.file_buffer.seek(std::io::SeekFrom::Start((self.xref_table[0] ) as u64))?;
-        let n = self.file_buffer.read(&mut self.mem_buffer)?;
-
-        let stream_start = self.bounded_seg_search(b"stream\r\n", 0, n)?;
-        let stream_end = self.bounded_seg_search(b"\r\nendstream", 0, n)?;
-
-
-        println!("{:?}",&self.xref_table);
-        
-        let mut decoder = ZlibDecoder::new(&self.mem_buffer[stream_start+8..stream_end]);
-        let mut dec_data = Vec::new();
-        
-        decoder.read_to_end(&mut dec_data);
-        println!("{}", String::from_utf8_lossy(&dec_data[..]));
-
+        self.mem_buffer = summary.into_bytes();
         Ok(&self.mem_buffer)
     }
 
-    fn bounded_seg_search(&self,key:&[u8], start:usize, end:usize)->Result<usize, std::io::Error>{
-        let x = self.mem_buffer[start..end].windows(key.len()).position(|x| x == key).expect("Unable to find start");
-        Ok(x)
+    fn metadata(&self) -> Result<ParserMetadata, Error> {
+        ParserMetadata::from_path(&self.path, "pdf")
     }
-    
-    fn nested_seg_search(&self, key:&[u8], start:usize, end:usize)->Result<Vec<usize>, std::io::Error>{
-        let x:Vec<usize> = self.mem_buffer[start..end].windows(key.len()).enumerate().filter(|(_, x)| *x == key).map(|(i,_)| i).collect();
-        Ok(x)
-    }
-
-    fn byte_to_ascii(&self, start:usize, end:usize)->Result<usize, std::io::Error>{ 
-        let mut ascii_value:usize = 0;
-        for i  in &self.mem_buffer[start..end]{
-            ascii_value = ascii_value*10 + (*i - b'0') as usize;
-        }
-        Ok(ascii_value)
-    }
-    
 }
 
-mod tests{
-    use crate::parser::pdf::{self, Pdf};
+mod tests {
+    use crate::parser::pdf::Pdf;
+    use crate::parser::Parser;
 
     #[test]
-    fn test() {
-        let pdf_file = Pdf::new("C:/Users/Vickynila/Projects/skeleton/data/test.pdf"); 
+    fn test_read_pdf() {
+        let manifest_dir = env!("CARGO_MANIFEST_DIR");
+        let path = format!("{}/../../data/test.pdf", manifest_dir);
 
-        if let Err(err) = pdf_file {
-            println!("{}", err);
-            panic!("Error");
-        }
-        
-        pdf_file.unwrap().read().ok();
+        let pdf_file = Pdf::new(&path);
+        assert!(pdf_file.is_ok(), "failed to open pdf file: {}", path);
 
+        let mut parser = pdf_file.unwrap();
+        let data = parser.read().expect("read failed");
+        assert!(!data.is_empty(), "parsed data should not be empty");
     }
-
 }
--- crates/core/src/parser/txt.rs
+++ crates/core/src/parser/txt.rs
@@ -1,96 +1,105 @@
-use std::io::{Error, Read};
 use std::fs::File;
-use std::io::{BufReader};
+use std::io::{BufReader, Error, Read};
 
-const SIZE:usize = 4096;
+use super::{Parser, ParserMetadata};
 
-struct Text{
+const SIZE: usize = 4096;
+
+pub struct Text {
+    path: String,
     file_buffer: BufReader<File>,
     mem_buffer: Vec<u8>,
     line_offset: usize,
     byte_offset: usize,
 }
 
-
-impl Text{
-    fn new(path:&str)-> Result<Text, Error>{
-
+impl Text {
+    fn open(path: &str) -> Result<Text, Error> {
         let file = File::open(path)?;
-
         let buffer = BufReader::new(file);
 
-        Ok(Text { file_buffer: buffer, mem_buffer:vec![0u8;SIZE], line_offset:0, byte_offset:0})
+        Ok(Text {
+            path: path.to_string(),
+            file_buffer: buffer,
+            mem_buffer: vec![0u8; SIZE],
+            line_offset: 0,
+            byte_offset: 0,
+        })
     }
 
-    fn read_line(&mut self) -> Result<&[u8],Error>{
-
+    fn read_line(&mut self) -> Result<&[u8], Error> {
         let mut start = 0;
         let mut end = 0;
         let mut found = false;
 
         loop {
-            
-            if self.line_offset == self.mem_buffer.len(){            
-                self.mem_buffer.resize(self.mem_buffer.len()+SIZE, 0u8); 
+            if self.line_offset == self.mem_buffer.len() {
+                self.mem_buffer.resize(self.mem_buffer.len() + SIZE, 0u8);
             }
-            
-            let n = self.file_buffer.read(&mut self.mem_buffer[self.line_offset..])?; 
+
+            let n = self.file_buffer.read(&mut self.mem_buffer[self.line_offset..])?;
             self.line_offset += n;
-            
-            println!("{},{}",self.byte_offset,self.line_offset);
-            
-            if self.line_offset <= self.byte_offset{
-                return Err(std::io::Error::from(
-                    std::io::ErrorKind::UnexpectedEof,
-                ));
+
+            if self.line_offset <= self.byte_offset {
+                return Err(std::io::Error::from(std::io::ErrorKind::UnexpectedEof));
             }
-            
-            
-            for i in self.byte_offset..self.line_offset{
-                if self.mem_buffer[i] == b'\n'{
+
+            for i in self.byte_offset..self.line_offset {
+                if self.mem_buffer[i] == b'\n' {
                     start = self.byte_offset;
                     end = i;
                     self.byte_offset = i + 1;
                     found = true;
-                    break ;
+                    break;
                 }
             }
-            
-            if found{
+
+            if found {
                 break;
             }
-            
         }
         Ok(&self.mem_buffer[start..end])
-        
     }
-    
 }
 
+impl Parser for Text {
+    fn new(path: &str) -> Result<Self, Error> {
+        Self::open(path)
+    }
+
+    fn read(&mut self) -> Result<&[u8], Error> {
+        self.read_line()
+    }
+
+    fn metadata(&self) -> Result<ParserMetadata, Error> {
+        ParserMetadata::from_path(&self.path, "text")
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use crate::parser::txt::Text;
+    use crate::parser::Parser;
 
     #[test]
-    fn test_sample(){
+    fn test_sample() {
         let txt_file = Text::new("C:/Users/Vickynila/Projects/skeleton/data/intro.txt");
-        if let Err(err) = &txt_file{
-            println!("{}",err) 
+        if let Err(err) = &txt_file {
+            println!("{}", err)
         }
         assert!(txt_file.is_ok());
     }
 
     #[test]
-    fn test_read_text(){
+    fn test_read_text() {
         let txt_file = Text::new("C:/Users/Vickynila/Projects/skeleton/data/intro.txt");
-        if let Err(err) = &txt_file{
-            println!("{}",err); 
+        if let Err(err) = &txt_file {
+            println!("{}", err);
             panic!("Error");
         }
         let mut txt_file = txt_file.unwrap();
-        while let Ok(value) = txt_file.read_line(){
-            println!("{:?}", &String::from_utf8(Vec::from(value))); 
+        while let Ok(value) = txt_file.read_line() {
+            println!("{:?}", &String::from_utf8(Vec::from(value)));
         }
     }
 }
