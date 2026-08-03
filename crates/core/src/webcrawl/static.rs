2fca3ae24652260928ceb7522fd8763e9ed9fadd
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-08-03 06:55:40 +0530

    Implemented initial version of static crawl

---
+++ crates/core/src/webcrawl/static.rs
@@ -0,0 +1,57 @@
+use crate::webcrawl::Error;
+use crate::webcrawl::ParserMetadata;
+use crate::webcrawl::WebParser;
+
+pub struct StaticPage {
+    pub url: String,
+    pub content: Vec<u8>,
+}
+
+impl StaticPage {
+    pub fn new(url: String, content: Vec<u8>) -> Self {
+        Self { url, content }
+    }
+}
+
+impl WebParser for StaticPage {
+    fn new(url: &str) -> Result<Self, Error> {
+        Ok(Self::new(url.to_string(), Vec::new()))
+    }
+
+    fn read(&mut self) -> Result<&[u8], Error> {
+        let content = std::str::from_utf8(&self.content).map_err(Error::from)?;
+        Ok(content.as_bytes())
+    }
+
+    fn metadata(&self) -> Result<ParserMetadata, Error> {
+        Ok(ParserMetadata::default())
+    }
+}
+
+
+#[test]
+fn test_static_page_new() {
+    let url = "http://example.com";
+    let content = b"<!DOCTYPE html><html><body><h1>Hello, World!</h1></body></html>";
+    let page = StaticPage::new(url, content);
+    assert_eq!(page.url, url);
+    assert_eq!(page.content, content);
+}
+
+#[test]
+fn test_static_page_read() {
+    let url = "http://example.com";
+    let content = b"<!DOCTYPE html><html><body><h1>Hello, World!</h1></body></html>";
+    let mut page = StaticPage::new(url, content);
+    let expected_content = content.to_vec();
+    assert_eq!(page.read().unwrap(), expected_content.as_ref());
+}
+
+#[test]
+fn test_static_page_metadata() {
+    let url = "http://example.com";
+    let content = b"<!DOCTYPE html><html><body><h1>Hello, World!</h1></body></html>";
+    let mut page = StaticPage::new(url, content);
+    let expected_metadata = ParserMetadata::default();
+    assert_eq!(page.metadata().unwrap(), expected_metadata);
+}
